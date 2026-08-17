54728a35a61b1e6f49308cb197879eb1f969c513
aetos <aetos382@users.noreply.github.com>
2026-08-17 12:18:35 +0900

    add test

--- Tests/LearnAsync.Tests.RuntimeAsync/Test1.cs
+++ Tests/LearnAsync.Tests.RuntimeAsync/Test1.cs
@@ -2,7 +2,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace LearnAsync.Tests;
+namespace LearnAsync.Tests.RuntimeAsync;
 
 [TestClass]
 public sealed class Test1
